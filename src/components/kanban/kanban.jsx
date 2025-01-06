3fc38caaf895f4def9ea8cad353eb64af6f4c0d0
rishabhdev2700 <rishavkumar2700@gmail.com>
2025-01-07 02:25:10 +0530

    added optimistic hook

--- src/components/kanban/kanban.jsx
+++ src/components/kanban/kanban.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import { cards, STATUS } from '../../lib/constants';
+import { startTransition, useEffect, useOptimistic } from 'react';
+import { STATUS } from '../../lib/constants';
 import { Column } from './column';
-import { Item } from './item';
+// import { Item } from './item';
 import {
   DndContext,
   closestCorners,
@@ -13,7 +13,7 @@ import {
 import { sortableKeyboardCoordinates } from '@dnd-kit/sortable';
 import { dataAPI } from '../../lib/data-api';
 
-export default function Kanban({items,update}) {
+export default function Kanban({ items, update }) {
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -23,10 +23,26 @@ export default function Kanban({items,update}) {
   useEffect(() => {
     update()
   }, [])
+  const [optimisticItems, addOptimisticItem] = useOptimistic(
+    items,
+    (state, newStatus) => {
+      return state.map(item =>
+        item.id === newStatus.cardID
+          ? { ...item, status: newStatus.columnID }
+          : item
+      );
+    }
+  );
 
   const handleDragEnd = async (event) => {
-    const cardID = event.active.data.current.id
-    const columnID = event.over.id
+    const cardID = event.active.data.current?.id
+    const columnID = event.over?.id
+    const item = items.filter(i => i.id === cardID)[0]
+    item.status = columnID
+    const optitem = items.filter(i => i.id !== cardID)
+    startTransition(() => {
+      addOptimisticItem([...optitem, item]);
+    })
     await dataAPI.updateCardStatus(cardID, { status: columnID })
     update()
   };
@@ -38,7 +54,7 @@ export default function Kanban({items,update}) {
       collisionDetection={closestCorners}
     >
       <div className='flex flex-col mr-8 lg:flex-row w-full justify-center'>
-        {STATUS.map((s, i) => <Column key={i} id={s.index} items={items} color={s.color} />)}
+        {STATUS.map((s, i) => <Column key={i} id={s.index} items={optimisticItems.filter((item) => item.status === s.index)} color={s.color} />)}
       </div>
     </DndContext>
   );
